206b6865026891877c68f18aaada7f9f0d961460
Fredrik Tell <FTell92@gmail.com>
2019-09-15 12:34:58 +0200

    Update doorbell

--- doorbell.py
+++ doorbell.py
@@ -1,6 +1,5 @@
 #coding=utf-8
-#author: fredrik.tell
-
+#author: Fredrik Tell
 from pushbullet import Pushbullet
 import wget
 import shutil
@@ -11,60 +10,53 @@ import requests
 import threading
 import sys
 
-
-#Google Home - https://github.com/GhostBassist/GooglePyNotify
-
-channel = "lerkil"
-api_key = "o.9NgIOV3yKFrMALNXKwFlPjlU2s2T02yd"
+pb_channel_name = "lerkil"
+pb_api_key = "o.9NgIOV3yKFrMALNXKwFlPjlU2s2T02yd"
 cameraImageUrl = "http://admin:mannaviken6@192.168.1.205/ISAPI/Streaming/channels/101/picture"
 cameraTwoWayUrl = "http://admin:mannaviken5@192.168.1.205/ISAPI/System/TwoWayAudio/channels/1/audioData"
 google_home_url = 'http://127.0.0.1/Notify?Doorbell'
-#google_home_url = 'http://127.0.0.1/Notify?Welcome'
-title = "Någon är vid dörren"
-message = "Lerkil - Ringklocka"
+message = "Visitor at the front door"
 outputFolder = "/home/pi/picture.jpg"
 GPIO_Pin = 5
 GPIO_Steady = 2000
 
-pb = Pushbullet(api_key)
-pb_channel = pb.get_channel(channel)
+pb = Pushbullet(pb_api_key)
+pb_channel = pb.get_channel(pb_channel_name)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(GPIO_Pin, GPIO.IN)
 
+#Send sound to Google Home
 def GoogleHomeNotice():
-    #Send sound to Google Home
     try:
         requests.get(google_home_url)
     except:
         print("ERROR - Can't send sound to Google Home")
 
+#Send Pushbullet with image
 def PushbulletNotice():
-    #Get image from camera
+    #Get image from camera and replace the old one
     filename = wget.download(cameraImageUrl, out=outputFolder)
     shutil.move(filename, outputFolder)
     filename = outputFolder
     
-    #Get current time
-    now = datetime.now() # current date and time
-    
-
-    #Send Pushbullet with image
+    #Upload image to Pushbullet server
     with open(filename, "rb") as pic:
         file_data = pb.upload_file(pic, "picture.jpg")
         
+    #Get current time
+    now = datetime.now() # current date and time
     date_time = now.strftime("%Y-%m-%d %H:%M")
-    global title
-    title_time = title + " " + date_time
-    pb_channel.push_file(body=title_time, title=message, **file_data)
     
+    #Publish notification
+    pb_channel.push_file(body=date_time, title=message, **file_data)
     
+#Send sound to Hikvision IP camera
 def DoorbellSpeakerNotice():
     headers = {'content-type': 'application/binary'}
     data = open('output.ulaw')
     requests.put(cameraTwoWayUrl, headers=headers, data=data)
 
-
-
+#Button event
 def Doorbell(channel):
     #Google Home notice
     GoogleHomeNoticeThread = threading.Thread(target=GoogleHomeNotice)
@@ -78,7 +70,6 @@ def Doorbell(channel):
     DoorbellSpeakerNoticeThread = threading.Thread(target=DoorbellSpeakerNotice) 
     DoorbellSpeakerNoticeThread.start()
     
- 
 
 GPIO.add_event_detect(GPIO_Pin, GPIO.FALLING, callback=Doorbell, bouncetime=GPIO_Steady)
 print("Doorbell is started")
@@ -86,5 +77,5 @@ while True:
     try:
         time.sleep(10)
     except KeyboardInterrupt:
-        print("Thanks for this time Mr. Tell!")
+        print("Thanks for this time!")
         sys.exit()
